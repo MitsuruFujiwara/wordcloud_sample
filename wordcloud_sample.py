af7391c5772bd86a9bac948b5e9fac477b81881b
MitsuruFujiwara <fujiwara52jp@gmail.com>
2018-04-21 09:34:18 +0900

    add data cleansing

--- wordcloud_sample.py
+++ wordcloud_sample.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 
 from wordcloud import WordCloud
 from nltk.corpus import stopwords
@@ -13,10 +14,12 @@ df = pd.read_csv('sample_text.csv')
 # generate text data
 txt = ' '.join(df['headline_text'])
 
+# cleansing data
+txt = re.sub('[\W]+', ' ', txt.lower())
+
 # generate wordcloud with stopwords
 wc = WordCloud(stopwords=stop).generate(txt)
 
 # save figure
 plt.imshow(wc)
-plt.axis("off")
-plt.savefig('wordcloud.png')
+wc.to_file('wordcloud.png')
